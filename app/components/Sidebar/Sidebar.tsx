d71f2534f7f6a8670618e005070fc42a06fa398b
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-25 21:56:21 +0100

    theme.ts

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react'
+import styled from 'styled-components'
 
 function Sidebar() {
   return (
@@ -9,4 +10,10 @@ function Sidebar() {
   )
 }
 
+const SidebarStyled = styled.nav`
+ padding:2.5rem;
+ display:flex;
+ gap:2.5rem;
+ height:100%;
+`
 export default Sidebar
