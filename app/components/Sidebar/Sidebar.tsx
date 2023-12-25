8d8e78409db97f35df820f503c34ea5f03266890
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-26 00:47:02 +0100

    adding the profile image

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -4,19 +4,25 @@
 import React from "react";
 import styled from "styled-components";
 import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
 
 function Sidebar() {
 	const { theme } = useGlobalState();
 	// console.log(theme);
 
-	return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+	return <SidebarStyled theme={theme}>
+    <div className="profile">
+      <div className="profile-overlay"></div>
+      <Image width={70} height={70} src="/images/profile.png" alt="profile" />
+    </div>
+  </SidebarStyled>;
 }
 
 const SidebarStyled = styled.nav`
 	position: relative;
 	width: ${(props) => props.theme.sidebarWidth};
 	background-color: ${(props) => props.theme.colorBg2};
-	border-right: 2px solid ${(props) => props.theme.borderColor2};
+	border: 2px solid ${(props) => props.theme.borderColor2};
 	border-radius: 1rem;
 `;
 export default Sidebar;
--- app/providers/GlobalStyleProvider.tsx
+++ app/providers/GlobalStyleProvider.tsx
@@ -1,24 +1,21 @@
+/** @format */
+
 "use client";
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-
-interface Props{
-    children: React.ReactNode;
+interface Props {
+	children: React.ReactNode;
 }
-function GlobalStyleProvider({children}:Props) {
-  return (
-    <GlobalStyles>
-      {children}
-    </GlobalStyles>
-  )
+function GlobalStyleProvider({ children }: Props) {
+	return <GlobalStyles>{children}</GlobalStyles>;
 }
 
 const GlobalStyles = styled.div`
- padding:2.5rem;
- display:flex;
- gap:2.5rem;
- height:100%;
-`
+	padding: 2.5rem;
+	display: flex;
+	gap: 2.5rem;
+	height: 100%;
+`;
 
-export default GlobalStyleProvider
+export default GlobalStyleProvider;
