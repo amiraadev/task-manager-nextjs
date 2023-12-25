e8c67df14f464eb2a09838b8752ea12b518780aa
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-25 21:24:26 +0100

    GlobalStyleProvider

---
+++ app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ app/providers/GlobalStyleProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react'
+import styled from 'styled-components';
+
+
+interface Props{
+    children: React.ReactNode;
+}
+function GlobalStyleProvider({children}:Props) {
+  return (
+    <GlobalStyles>
+      {children}
+    </GlobalStyles>
+  )
+}
+
+const GlobalStyles = styled.div`
+ padding:2.5rem;
+ display:flex;
+ gap:2.5rem;
+ height:100%;
+`
+
+export default GlobalStyleProvider
