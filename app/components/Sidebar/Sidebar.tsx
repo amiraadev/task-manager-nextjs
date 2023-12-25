b96454de66af83fabddc485874c6d283fb670ec3
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-26 00:53:19 +0100

    adding /utils/menu.js

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -10,12 +10,28 @@ function Sidebar() {
 	const { theme } = useGlobalState();
 	// console.log(theme);
 
-	return <SidebarStyled theme={theme}>
-    <div className="profile">
-      <div className="profile-overlay"></div>
-      <Image width={70} height={70} src="/images/profile.png" alt="profile" />
-    </div>
-  </SidebarStyled>;
+	return (
+		<SidebarStyled theme={theme}>
+			<div className='profile'>
+				<div className='profile-overlay'></div>
+				<div className='image'>
+					<Image
+						width={70}
+						height={70}
+						src='/images/profile.png'
+						alt='profile'
+					/>
+				</div>
+        <span>
+           Amira
+        </span> 
+        <span>
+           Allagui
+        </span>
+			</div>
+      <ul className="nav-items"></ul>
+		</SidebarStyled>
+	);
 }
 
 const SidebarStyled = styled.nav`
