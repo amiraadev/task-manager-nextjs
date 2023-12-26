71e4a2a2f13ad661dcaef92442e0601c31e41816
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-26 01:19:20 +0100

    mapping the links in the sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -5,11 +5,17 @@ import React from "react";
 import styled from "styled-components";
 import { useGlobalState } from "@/app/context/globalProvider";
 import Image from "next/image";
+import menu from "@/app/utils/menu";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 function Sidebar() {
+	const router = useRouter();
+	const pathname = usePathname();
 	const { theme } = useGlobalState();
-	// console.log(theme);
-
+	const handleClick = (link: string) => {
+		router.push(link);
+	};
 	return (
 		<SidebarStyled theme={theme}>
 			<div className='profile'>
@@ -22,14 +28,21 @@ function Sidebar() {
 						alt='profile'
 					/>
 				</div>
-        <span>
-           Amira
-        </span> 
-        <span>
-           Allagui
-        </span>
+				<span>Amira</span>
+				<span>Allagui</span>
 			</div>
-      <ul className="nav-items"></ul>
+			<ul className='nav-items'>
+				{menu.map((item, i) => {
+					return (
+						<li key={i} className={`nav-item ${pathname === item.link ? "active" :" "}`} onClick={() => {
+              handleClick(item.link)
+            }}>
+							{item.icon}
+							<Link href={item.link}>{item.title}</Link>
+						</li>
+					);
+				})}
+			</ul>
 		</SidebarStyled>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,15 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
+			<head>
+				<link
+					rel='stylesheet'
+					href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+					integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
+					crossOrigin='anonymous'
+					referrerPolicy='no-referrer'
+				/>
+			</head>
 			<body className={inter.className}>
 				<ContextProvider>
 					<GlobalStyleProvider>
